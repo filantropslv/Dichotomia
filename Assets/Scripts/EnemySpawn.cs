2f22e039d4bdb0b53576ee22d7e7a19ac41c759c
Ronalds Sovas <sovas@greenbox.lv>
2022-01-29 23:31:30 +0200

    index of respawning enemies

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -8,6 +8,7 @@ public class EnemySpawn : MonoBehaviour
     public int xPos;
     public int yPos;
     public int enemyCount;
+    public int levelIndex = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +19,14 @@ public class EnemySpawn : MonoBehaviour
     {
         while (true)
         {
-            if (enemyCount < 5)
+            if (enemyCount < (int)(levelIndex / 20))
             {
                 xPos = Random.Range(-3, 7);
                 yPos = Random.Range(1, 8);
 
                 Instantiate(theEnemy, new Vector3(xPos, yPos, 0), Quaternion.identity);
                 enemyCount += 1;
+                levelIndex += 2;
             }
 
             yield return new WaitForSeconds(0.5f);
